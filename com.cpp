1fc9692d6bd4ac4a250361ae86b361e582931f03
GuilhermeCintas <guilhermecintas1@gmail.com>
2020-05-26 19:26:53 -0300

    Primeira importacao

---
+++ com.cpp
@@ -0,0 +1,28 @@
+#include "com.h"
+#include <iostream>
+
+using namespace std;
+
+Comunication::Comunication(){
+
+};
+
+void Comunication::changesemaph(bool value){
+    this->semaph=value;
+};
+
+void Comunication::imprime(){
+	this->changesemaph(false);
+    std::cout << "Estado semáforo:"<< this->semaph << std::endl;
+    
+};
+
+bool Comunication::isAvailable(){
+	/*if(this->semaph == 1){
+		return true;
+	}else{
+		return false;
+	};*/
+	return (this->semaph);
+};
+
